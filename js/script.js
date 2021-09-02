1a834ccd3daff91ffd5550ceff64f8d31215a9b2
sajidulhasan01 <85543711+sajidulhasan01@users.noreply.github.com>
2021-09-02 12:41:28 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function onClickChange() {
+    document.getElementById('h3_1').innerHTML = "Welcome to SSB";
+}
+
+function dblClickChange() {
+    document.getElementById('h3_2').innerHTML = "Welcome to SSB";
+}
+
+function hoverChange() {
+    document.getElementById('h3_3').innerHTML = "Welcome to SSB";
+}
+
+function mouseOutChange() {
+    document.getElementById('h3_4').innerHTML = "Welcome to SSB";
+}
+
+function onClickColorChange() {
+    document.getElementById('h3_5').style.color = "blue";
+}
+
+function onClickStyleChange() {
+    document.getElementById('h3_6').style.fontStyle = "italic";
+    document.getElementById('h3_6').style.backgroundColor = "yellow";
+}
